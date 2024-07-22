1c57608c52766be0015d6f00736e55aea38b9a1f
Okan Emre KOCA <oknakoca@gmail.com>
2024-07-22 09:25:27 +0200

    display account info

--- cpp-00/ex02/Account.cpp
+++ cpp-00/ex02/Account.cpp
@@ -6,11 +6,14 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/21 21:39:22 by okoca             #+#    #+#             */
-/*   Updated: 2024/07/21 22:05:51 by okoca            ###   ########.fr       */
+/*   Updated: 2024/07/22 08:56:27 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <string>
 
 int	Account::getNbAccounts()
 {
@@ -34,25 +37,57 @@ int	Account::getNbWithdrawals()
 
 void	Account::makeDeposit(int deposit)
 {
-	this->_totalAmount += deposit;
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	_totalAmount += deposit;
+	_totalNbDeposits++;
 }
 
 bool	Account::makeWithdrawal(int withdrawal)
 {
-	if (withdrawal > this->_totalAmount)
+	if (withdrawal > this->_amount)
 		return (false);
-	this->_totalAmount -= withdrawal;
+	this->_amount -= withdrawal;
+	this->_nbWithdrawals++;
+	_totalAmount -= withdrawal;
+	_totalNbWithdrawals++;
 	return (true);
 }
 
 int	Account::checkAmount() const
 {
-	return (_totalAmount);
+	return (this->_amount);
 }
 
 Account::Account(int initial_deposit)
 {
-	this->makeDeposit(initial_deposit);
+	this->_accountIndex = _nbAccounts;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	this->_amount = initial_deposit;
+	_totalAmount += initial_deposit;
+	_nbAccounts++;
+}
+
+void	Account::_displayTimestamp()
+{
+	std::tm	time;
+	std::get_time(&time, "%Y%m%d_%H%M%S");
+	std::cout << std::put_time(&time, "%c");
+}
+
+
+// [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+void	Account::displayAccountsInfos()
+{
+	Account::_displayTimestamp();
+	std::cout << Account::getNbAccounts() << ";" << Account::getTotalAmount() << ";"
+		<< Account::getNbDeposits() << ";" << Account::getNbWithdrawals() << std::endl;
+}
+
+void	Account::displayStatus() const
+{
+
 }
 
 Account::~Account()
