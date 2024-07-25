41590278757704d8be8ba1a1d45b82e052f95a29
Okan Emre KOCA <oknakoca@gmail.com>
2024-07-25 15:55:20 +0200

    canonical form complete

--- cpp-04/ex03/Character.cpp
+++ cpp-04/ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/25 15:44:53 by okoca             #+#    #+#             */
-/*   Updated: 2024/07/25 15:45:12 by okoca            ###   ########.fr       */
+/*   Updated: 2024/07/25 15:55:07 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,51 @@
 
 Character::Character()
 {
+	this->total = 0;
+	this->name = "No name adventurer";
+	this->materias = new AMateria*[4];
+	for (int i = 0; i < 4; i++)
+		this->materias[i] = NULL;
+	std::cout << this->name << " has signed up as an adventurer" << std::endl;
+}
 
+Character::Character(const std::string & name)
+{
+	this->total = 0;
+	this->name = name;
+	this->materias = new AMateria*[4];
+	for (int i = 0; i < 4; i++)
+		this->materias[i] = NULL;
+	std::cout << this->name << " has signed up as an adventurer" << std::endl;
 }
 
 Character::Character(const Character &value)
 {
-
+	this->total = value.total;
+	this->name = value.name;
+	this->materias = new AMateria*[4];
+	for (int i = 0; i < 4; i++)
+		this->materias[i] = value.materias[i];
+	std::cout << this->name << " is a new student of " << value.name << std::endl;
 }
 
 Character::~Character()
 {
-
+	for (int i = 0; i < 4; i++)
+		delete this->materias[i];
+	delete[] this->materias;
+	std::cout << this->name << " has been killed in battle.." << std::endl;
 }
 
 Character & Character::operator=(const Character &value)
 {
 	if (this != &value)
 	{
-		// Copy data members from value to this object
+		this->total = value.total;
+		this->name = value.name;
+		this->materias = new AMateria*[4];
+		for (int i = 0; i < 4; i++)
+			this->materias[i] = value.materias[i];
 	}
 	return *this;
 }
