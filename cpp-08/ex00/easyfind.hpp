a708a3756a5c062342b7aca947258ebda7ef896c
Okan Emre KOCA <oknakoca@gmail.com>
2024-08-08 10:40:03 +0200

    did associative too

--- cpp-08/ex00/easyfind.hpp
+++ cpp-08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 08:52:41 by okoca             #+#    #+#             */
-/*   Updated: 2024/08/08 09:15:09 by okoca            ###   ########.fr       */
+/*   Updated: 2024/08/08 10:39:57 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 #include <algorithm>
 
 template <typename T>
-int	&easyfind(T& container, int number)
+int	easyfind(T& container, int number)
 {
 	for (size_t i = 0; i < container.size(); i++)
 	{
@@ -28,6 +28,19 @@ int	&easyfind(T& container, int number)
 	// decided to throw error since we return an integer,
 	// and if the value that is searched is is the INT_MAX
 	// we wouldnt have a way to check if value is found/or not.
+	throw std::logic_error("Couldn't find value.");
+}
+
+// associative containers -> need C++11 to only have one function.
+template <typename T>
+typename T::key_type	easyfind_map(T& container, int number)
+{
+	typename T::iterator ptr;
+	for (ptr = container.begin(); ptr != container.end(); ptr++)
+	{
+		if (ptr->second == number)
+			return (ptr->first);
+	}
 	throw std::logic_error("Couldn't find value.");
 }
 
--- cpp-08/ex00/main.cpp
+++ cpp-08/ex00/main.cpp
@@ -6,29 +6,46 @@
 /*   By: okoca <okoca@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/08 08:48:12 by okoca             #+#    #+#             */
-/*   Updated: 2024/08/08 09:14:19 by okoca            ###   ########.fr       */
+/*   Updated: 2024/08/08 10:39:05 by okoca            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <cstdlib>
 
 #include "easyfind.hpp"
 
 int main(int ac, char **av)
 {
-	std::vector<int> vec;
-
-	for (int i = 0; i < 10; i++)
-		vec.push_back(i);
+	int find = 5;
+	if (ac == 2)
+		find = std::atoi(av[1]);
 	try
 	{
-		int find = 5;
-		if (ac == 2)
-			find = std::atoi(av[1]);
+		std::vector<int> vec;
+
+		for (int i = 0; i < 10; i++)
+			vec.push_back(i);
 		std::cout << easyfind(vec, find) << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
+	try
+	{
+		std::map<std::string, int> map;
+		map["hello"] = 10;
+		map["wow"] = 5;
+		map["haha"] = 2;
+		map["cool"] = 25;
+		map["hello world!"] = 89123;
+		map["haha another element"] = 79;
+		std::cout << easyfind_map(map, find) << std::endl;
 	}
 	catch(const std::exception& e)
 	{
